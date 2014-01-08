d9cd1008b35e0a5f9a311bf1b35d80e6f1d7cabe
johnvint <johnvintc3@gmail.com>
2014-01-08 08:36:45 -0500

    Update PdfToPPT.java

--- src/com/vint/pdf2ppt/core/PdfToPPT.java
+++ src/com/vint/pdf2ppt/core/PdfToPPT.java
@@ -21,29 +21,30 @@ import org.apache.poi.xslf.usermodel.XSLFSlide;
 
 public class PdfToPPT implements Processor {
 
-	@Override
-	public void process(File input, File output) throws FileNotFoundException, IOException {
-		PDDocument doc = PDDocument.load(input);
-		List<PDPage> pages = doc.getDocumentCatalog().getAllPages();
-		Iterator iter = pages.iterator();
-		XMLSlideShow ppt = new XMLSlideShow();
-		java.awt.Dimension pgsize = ppt.getPageSize();
+    @Override
+    @SuppressWarnings("unchecked")
+    public void process(File input, File output) throws FileNotFoundException, IOException {
+        PDDocument doc = PDDocument.load(input);
+        List<PDPage> pages = doc.getDocumentCatalog().getAllPages();
+        Iterator<PDPage> iter = pages.iterator();
+        XMLSlideShow ppt = new XMLSlideShow();
+        java.awt.Dimension pgsize = ppt.getPageSize();
 
-		while (iter.hasNext()) {
-			XSLFSlide slide = ppt.createSlide();
-			PDPage page = (PDPage) iter.next();
-			ByteArrayOutputStream baos = new ByteArrayOutputStream();
-			ImageIO.write(page.convertToImage(BufferedImage.TYPE_INT_RGB, 400), "jpg", baos);
-			baos.flush();
-			byte[] imageInByte = baos.toByteArray();
-			System.out.println(imageInByte.length);
+        while (iter.hasNext()) {
+            XSLFSlide slide = ppt.createSlide();
+            PDPage page =  iter.next();
+            ByteArrayOutputStream baos = new ByteArrayOutputStream();
+            ImageIO.write(page.convertToImage(BufferedImage.TYPE_INT_RGB, 400), "jpg", baos);
+            baos.flush();
+            byte[] imageInByte = baos.toByteArray();
+            System.out.println(imageInByte.length);
 
-			int idx = ppt.addPicture(imageInByte, XSLFPictureData.PICTURE_TYPE_JPEG);
-			XSLFPictureShape pic = slide.createPicture(idx);
-			pic.setAnchor(new Rectangle2D.Double(0, 0, pgsize.width + 50, pgsize.height));
-		}
-		FileOutputStream out = new FileOutputStream("merged.ppt");
-		ppt.write(out);
-		out.close();
-	}
+            int idx = ppt.addPicture(imageInByte, XSLFPictureData.PICTURE_TYPE_JPEG);
+            XSLFPictureShape pic = slide.createPicture(idx);
+            pic.setAnchor(new Rectangle2D.Double(-50, 0, pgsize.width * 1.15, pgsize.height * 1.10));
+        }
+        FileOutputStream out = new FileOutputStream("merged.ppt");
+        ppt.write(out);
+        out.close();
+    }
 }
