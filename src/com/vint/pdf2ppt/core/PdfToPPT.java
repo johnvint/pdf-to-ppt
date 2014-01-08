c030093467e61571ac2de05e49d1b5c140f3c248
johnvint <johnvintc3@gmail.com>
2014-01-08 16:20:47 -0500

    Fix out of memory bug

--- src/com/vint/pdf2ppt/core/PdfToPPT.java
+++ src/com/vint/pdf2ppt/core/PdfToPPT.java
@@ -1,5 +1,7 @@
 package com.vint.pdf2ppt.core;
 
+import java.awt.Graphics2D;
+import java.awt.RenderingHints;
 import java.awt.geom.Rectangle2D;
 import java.awt.image.BufferedImage;
 import java.io.ByteArrayOutputStream;
@@ -7,7 +9,6 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
-import java.util.Iterator;
 import java.util.List;
 
 import javax.imageio.ImageIO;
@@ -19,32 +20,52 @@ import org.apache.poi.xslf.usermodel.XSLFPictureData;
 import org.apache.poi.xslf.usermodel.XSLFPictureShape;
 import org.apache.poi.xslf.usermodel.XSLFSlide;
 
-public class PdfToPPT implements Processor {
+public class PdfToPpt implements Processor {
 
     @Override
     @SuppressWarnings("unchecked")
     public void process(File input, File output) throws FileNotFoundException, IOException {
         PDDocument doc = PDDocument.load(input);
         List<PDPage> pages = doc.getDocumentCatalog().getAllPages();
-        Iterator<PDPage> iter = pages.iterator();
+        File file = new File("merged.ppt");
         XMLSlideShow ppt = new XMLSlideShow();
         java.awt.Dimension pgsize = ppt.getPageSize();
 
-        while (iter.hasNext()) {
+        for (PDPage page : pages) {
             XSLFSlide slide = ppt.createSlide();
-            PDPage page =  iter.next();
+
             ByteArrayOutputStream baos = new ByteArrayOutputStream();
-            ImageIO.write(page.convertToImage(BufferedImage.TYPE_INT_RGB, 400), "jpg", baos);
+            BufferedImage bi = page.convertToImage(BufferedImage.TYPE_INT_RGB, 400);
+            BufferedImage image = resize(bi, pgsize.width * 1.15, pgsize.height * 1.10);
+            ImageIO.write(image, "jpg", baos);
             baos.flush();
             byte[] imageInByte = baos.toByteArray();
-            System.out.println(imageInByte.length);
 
             int idx = ppt.addPicture(imageInByte, XSLFPictureData.PICTURE_TYPE_JPEG);
             XSLFPictureShape pic = slide.createPicture(idx);
-            pic.setAnchor(new Rectangle2D.Double(-50, 0, pgsize.width * 1.15, pgsize.height * 1.10));
+            pic.setAnchor(new Rectangle2D.Double(-70, 0, pgsize.width, pgsize.height));
+            
+            // this is necessary
+            // the BufferedImage leverages a native byte array that may not get
+            // automatically gc'd before the next one is created.  
+            // for now we will brute force it to work
+            System.gc();
         }
-        FileOutputStream out = new FileOutputStream("merged.ppt");
-        ppt.write(out);
-        out.close();
+
+        FileOutputStream fOut = new FileOutputStream(file);
+        ppt.write(fOut);
+        fOut.close();
     }
+
+    public BufferedImage resize(BufferedImage img, double newW, double newH) {
+        int w = img.getWidth();
+        int h = img.getHeight();
+        BufferedImage dimg = new BufferedImage((int) newW, (int) newH, img.getType());
+        Graphics2D g = dimg.createGraphics();
+        g.setRenderingHint(RenderingHints.KEY_INTERPOLATION, RenderingHints.VALUE_INTERPOLATION_BILINEAR);
+        g.drawImage(img, 0, 0, (int) newW, (int) newH, 0, 0, w, h, null);
+        g.dispose();
+        return dimg;
+    }
+
 }
